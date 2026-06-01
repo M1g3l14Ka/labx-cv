03dc29b09b86135b8680667b3466e5a75bdddfdc
M1g3l14Ka <katcion00@mail.ru>
2026-06-01 16:45:15 +0300

    update user_info

--- src/app/actions/askAI.ts
+++ src/app/actions/askAI.ts
@@ -16,11 +16,19 @@ export async function askAI(userQuestion: string) {
     
     Информация о Михаиле:
     - Уровень: Junior+ / Pre-Middle Fullstack.
-    - Стек: Next.js 16 (App Router), React 19, TypeScript, Tailwind CSS v4, Prisma, Node.js.
-    - Подход к ИИ: Михаил использует ИИ как высокоточный станок ЧПУ, выступая в роли архитектора и оператора.
-    - Крутые проекты: "Ventala" (генерация тысяч SKU для 1С-Битрикс на Node.js), "Cleantech" (SEO автоматизация).
+    - Стек: Core: JavaScript (ES6+), TypeScript, Frontend: React 19, Next.js 16, State & Data: Zustand, React Hook Form, Zod, Backend & DB: Node.js, SQLite, Prisma ORM, NextAuth, Styling & UI: Tailwind CSS v4, shadcn/ui, Framer Motion, AI & Tools: Gemini API, Generative UI, Figma, Git, Vercel/Свой Linux сервер 
+    - Подход к ИИ: Михаил использует ИИ как высокоточный станок ЧПУ и хорошего друга, выступая в роли архитектора и оператора.
     - Любимая шутка про ошибки: "418 I'm a teapot - единственная ошибка, которую я пока не получал в продакшене".
-    - Хобби: CS2, PUBG, MLBB и поглощение тонн кофе.
+    - Хобби: CS2, PUBG, Atomic Heart, terraria/Calamity/Infernum   и поглощение тонн кофе.
+    - Полезные ссылки или основные проекты:
+      - резюме: https://github.com/M1g3l14Ka/web-resume / https://resume.michaelkasion.ru/
+      - небольшой магазинчик (тренировка работы с dummyJson) : https://github.com/M1g3l14Ka/lite-shop / https://shop.michaelkasion.ru/
+      - второе резюме: https://github.com/M1g3l14Ka/labx-cv / https://labx.michaelkasion.ru/
+      - парсер данных с Корейского сайта по продаже авто: https://github.com/M1g3l14Ka/sellercars / https://sellercars.michaelkasion.ru/
+    - Коммерчиские проекты:
+      - "Ventala" (генерация тысяч SKU для 1С-Битрикс на Node.js)
+      - "Cleantech" (SEO автоматизация)
+      - своя crm для учета финансов: https://github.com/M1g3l14Ka/freelance-crm / https://crm.michaelkasion.ru
 
     Ответь на следующий вопрос пользователя опираясь на эту информацию: "${userQuestion}"
     `;
